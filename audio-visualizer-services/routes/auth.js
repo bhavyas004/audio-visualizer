82ef088601082c89dbc9949f543cd98f3a4ee21f
bhavyas004 <162615572+bhavyas004@users.noreply.github.com>
2025-03-05 17:20:12 +0530

    routes added to api

---
+++ audio-visualizer-services/routes/auth.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+const router = express.Router();
+const JWT_SECRET = 'AudioVisualizer4WE';
+
+// Register route
+router.post('/register', async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const isMatch = await bcrypt.compare(password, hashedPassword);
+    console.log('Password:', password);
+    console.log('Hashed Password:', hashedPassword);
+    console.log('isMath:', isMatch);
+    const user = new User({ name, email, password });
+    await user.save();
+    res.status(201).send('User registered successfully');
+  } catch (error) {
+    res.status(400).send('Error registering user: ' + error.message);
+  }
+});
+
+// Login route
+router.post('/login', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ message: 'Invalid email or password' });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    console.log('Password:', password);
+    console.log('Hashed Password:', user.password);
+    console.log('isMatch:', isMatch);
+    if (!isMatch) {
+      return res.status(401).json({ message: 'Invalid email or password' });
+    }
+    const token = jwt.sign({ userId: user._id }, JWT_SECRET, { expiresIn: '1h' });
+    res.status(200).json({ token });
+  } catch (error) {
+    res.status(400).send('Error logging in: ' + error.message);
+  }
+});
+
+module.exports = router;
